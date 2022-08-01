abd3277c51834099427345286b8cb571b568207b
Maxime Bellec <maxime@telescoop.fr>
2022-08-01 11:30:42 +0200

    [+] backup: add postgresl dump support

--- telescoop_backup/backup.py
+++ telescoop_backup/backup.py
@@ -8,22 +8,28 @@ from django.conf import settings
 import boto
 from boto.s3.key import Key
 
+IS_POSTGRES = "postgresql" in settings.DATABASES["default"]["ENGINE"]
 
 DATE_FORMAT = "%Y-%m-%dT%H:%M"
-FILE_FORMAT = f"{DATE_FORMAT}_db.sqlite"
 DEFAULT_AUTH_VERSION = 2
 DEFAULT_CONTAINER_NAME = "db-backups"
-db_file_path = settings.DATABASES["default"]["NAME"]
-DATABASE_BACKUP_FILE = os.path.join(os.path.dirname(db_file_path), "backup.sqlite")
-DUMP_COMMAND = """sqlite3 '{source_file}' ".backup '{backup_file}'" """.format(
-    source_file=db_file_path, backup_file=DATABASE_BACKUP_FILE
-)
+if IS_POSTGRES:
+    DATABASE_BACKUP_FILE = os.path.join(settings.BASE_DIR, "dump.sql")
+    FILE_FORMAT = f"{DATE_FORMAT}_postgres_dump.sql"
+else:
+    db_file_path = settings.DATABASES["default"]["NAME"]
+    DATABASE_BACKUP_FILE = os.path.join(os.path.dirname(db_file_path), "backup.sqlite")
+    SQLITE_DUMP_COMMAND = """sqlite3 '{source_file}' ".backup '{backup_file}'" """.format(
+        source_file=db_file_path, backup_file=DATABASE_BACKUP_FILE
+    )
+    FILE_FORMAT = f"{DATE_FORMAT}_db.sqlite"
 KEEP_N_DAYS = getattr(settings, "BACKUP_KEEP_N_DAYS", 31)
 host = getattr(settings, "BACKUP_HOST", None)
 if host is None:
     region = getattr(settings, "BACKUP_REGION", "eu-west-3")
     host = f"s3.{region}.amazonaws.com"
 LAST_BACKUP_FILE = os.path.join(settings.BASE_DIR, ".telescoop_backup_last_backup")
+
 
 
 def boto_connexion():
@@ -79,7 +85,17 @@ def get_backup_bucket(connexion=None):
 
 def dump_database():
     """Dump the database to a file."""
-    subprocess.check_output(DUMP_COMMAND, shell=True)
+    if IS_POSTGRES:
+        import pexpect
+        db_name = settings.DATABASES["default"]["NAME"]
+        db_user = settings.DATABASES["default"]["USER"]
+        db_password = settings.DATABASES["default"]["PASSWORD"]
+        shell_cmd = f"pg_dump {db_name} -U {db_user} > {DATABASE_BACKUP_FILE}"
+        child = pexpect.spawn("/bin/bash", ["-c", shell_cmd])
+        child.expect("Password:")
+        child.sendline(db_password)
+    else:
+        subprocess.check_output(SQLITE_DUMP_COMMAND, shell=True)
 
 
 def remove_old_database_files():
@@ -141,6 +157,8 @@ def get_backup_keys(connexion=None):
 
 
 def recover_database(db_file):
+    if IS_POSTGRES:
+        raise NotImplementedError()
     # download latest db_file
     bucket = get_backup_bucket()
     key = bucket.get_key(db_file)
@@ -165,23 +183,3 @@ def list_saved_databases():
 
 def db_name() -> str:
     return datetime.datetime.now().strftime(FILE_FORMAT)
-
-
-def parse_db_date_from_file_name(file_name: str) -> datetime.datetime:
-    return datetime.datetime.strptime(file_name, FILE_FORMAT)
-
-
-def retrieve_openstack_configuration():
-    """Retrieve configuaration from settings.py."""
-    try:
-        url = getattr(settings, "BACKUP_AUTH_URL")
-        user = getattr(settings, "BACKUP_USER")
-        key = getattr(settings, "BACKUP_KEY")
-        tenant_name = getattr(settings, "BACKUP_TENANT_NAME")
-        auth_version = getattr(settings, "BACKUP_AUTH_VERSION", 2)
-        container_name = getattr(settings, "BACKUP_CONTAINER_NAME")
-    except AttributeError as e:
-        missing_attribute = e.args[0].split("attribute ")[1]
-        raise AttributeError(f"You must define '{missing_attribute}' in your settings.")
-
-    return url, user, key, tenant_name, auth_version, container_name
