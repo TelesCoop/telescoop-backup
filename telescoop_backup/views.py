310259506fac2324f61866dc7cd252994254b131
Maxime Bellec <maxime@telescoop.fr>
2024-01-09 12:58:47 +0100

    fix: change backup check, was wrong by a factor of 24!

--- telescoop_backup/views.py
+++ telescoop_backup/views.py
@@ -8,7 +8,7 @@ from telescoop_backup.backup import get_latest_backup, DATE_FORMAT
 def check_backup_is_recent(request, hours):
     last_backup = get_latest_backup()
     now = datetime.now()
-    if last_backup and (now - last_backup).total_seconds() / 3600 / 24 < hours:
+    if last_backup and (now - last_backup).total_seconds() / 3600 < hours:
         return HttpResponse("yes", status=200)
     else:
         return HttpResponse("no", status=500)
